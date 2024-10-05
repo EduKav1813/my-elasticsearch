3f009e5dd09b524a88ff26d8c1765949ee870205
Eduard Kaverinskyi <KaverinskyiEduard1813@gmail.com>
2024-10-05 19:35:44 +0200

    Implement test for find_all_documents

--- src/my_elasticsearch/db.py
+++ src/my_elasticsearch/db.py
@@ -9,6 +9,10 @@ class Document(BaseModel):
     content: str = Field(...)
 
 
+def dict_to_document_mapper(db_document: dict) -> Document:
+    return Document(name=db_document["name"], content=db_document["content"])
+
+
 class DatabaseClient:
 
     def __init__(
@@ -26,17 +30,33 @@ class DatabaseClient:
         if self.__getattribute__("client"):
             self.client.close()
 
+    def drop_collection(self, collection=None):
+        if not collection:
+            collection = self.default_collection
+        self.database[collection].drop()
+
     def insert_document(self, document: Document, collection: str = None) -> None:
         if not collection:
             collection = self.default_collection
         self.database[collection].insert_one(document.model_dump())
 
-    def get_document(self, document_name: str, collection: str = None) -> Document:
+    def insert_documents(
+        self, documents: List[Document], collection: str = None
+    ) -> None:
         if not collection:
             collection = self.default_collection
-        return self.database[collection].find_one({"name": document_name})
+        documents_dump = [doc.model_dump() for doc in documents]
+        self.database[collection].insert_many(documents_dump)
 
-    def get_all_documents(self, collection: str = None) -> List[Document]:
+    def find_document(self, document_name: str, collection: str = None) -> Document:
         if not collection:
             collection = self.default_collection
-        return self.database[collection].find_all()
+        document = self.database[collection].find_one({"name": document_name})
+        return dict_to_document_mapper(document)
+
+    def find_all_documents(self, collection: str = None) -> List[Document]:
+        if not collection:
+            collection = self.default_collection
+        documents_list = self.database[collection].find().to_list()
+
+        return list(map(dict_to_document_mapper, documents_list))
--- tests/test_db.py
+++ tests/test_db.py
@@ -14,12 +14,27 @@ def setup_data():
         db_name="TESTDB",
         default_collection="TEST_COLLECTION",
     )
-
+    client.drop_collection()
     return {"client": client}
 
 
-def test_insert(setup_data):
+def test_find(setup_data):
     client: DatabaseClient = setup_data["client"]
-    document = Document(name="test_document", content="test_content")
+    document = Document(name="test_document_to_find", content="test_content")
     client.insert_document(document)
-    assert True
+
+    found_document = client.find_document(document_name=document.name)
+    assert document == found_document
+
+
+def test_find_many(setup_data):
+    client: DatabaseClient = setup_data["client"]
+    documents = [
+        Document(name="first_document", content="test_content"),
+        Document(name="second_document", content="test_content2"),
+    ]
+    client.insert_documents(documents)
+
+    found_documents = client.find_all_documents()
+
+    assert documents == found_documents

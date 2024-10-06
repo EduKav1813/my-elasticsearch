88e22262e5b89164d6315b77898517a583133ead
Eduard Kaverinskyi <KaverinskyiEduard1813@gmail.com>
2024-10-06 20:28:05 +0200

    Implement basic CLI with fire

---
+++ src/my_elasticsearch/cli.py
@@ -0,0 +1,106 @@
+import dotenv
+import fire
+
+from my_elasticsearch.db import DatabaseClient, Document
+from my_elasticsearch.searchengine import SearchEngine
+
+
+class Cli:
+
+    def __init__(self) -> None:
+        dotenv_data = dotenv.dotenv_values(dotenv.find_dotenv())
+        self.default_collection = dotenv_data["COLLECTION_NAME"]
+        self.db_client = DatabaseClient(
+            hostname=dotenv_data["MONGODB_HOST"],
+            port=int(dotenv_data["MONGODB_PORT"]),
+            db_name=dotenv_data["DB_NAME"],
+            default_collection=self.default_collection,
+        )
+        self.search_engine = SearchEngine()
+
+    def insert_document(self, name: str, content: str, collection: str = None):
+        """Insert a single document into specified collection.
+
+        Args:
+            name (str): Name of the document.
+            content (str): Content of the document.
+            collection (str, optional): _description_. Defaults to None, in
+                which case the "COLLECTION_NAME" from .env will be used.
+
+        Raises:
+            RuntimeError: Raised if inserting the document fails.
+        """
+        if not collection:
+            collection = self.default_collection
+        try:
+            document = Document(name=name, content=content)
+            self.db_client.insert_document(document)
+            print(f"Inserted document with {name=}, {content=} into {collection=}.")
+        except Exception as e:
+            raise RuntimeError(
+                f"Cannot insert document with {name=}, {content=} into {collection=}. Exception: {e}"
+            )
+
+    def drop_collection(self, collection: str = None):
+        """Drops the specified or default collection.
+
+        Args:
+            collection (str, optional): Name of the collection to be dropped.
+                Defaults to 'COLLECTION_NAME' from .env .
+        """
+        self.db_client.drop_collection(collection)
+
+    def search(
+        self, query: str, collection: str = None, limit: int = None, debug_score=False
+    ) -> None:
+        """Search among the documents from specified collection with a query.
+        Prints results in a descending list of all matches.
+
+        Args:
+            query (str): Query for search.
+            collection (str, optional): Name of the collections. Defaults to 'COLLECTION_NAME' from .env.
+            limit (int, optional): Output limit.. Defaults to None.
+
+            debug_score (bool): Debug option to print scores next to the result.
+                Defaults to False.
+
+        Raises:
+            RuntimeError: Raised if there are no documents in the collection to
+            search through.
+        """
+        documents = self.db_client.find_all_documents(collection)
+
+        if len(documents) == 0:
+            raise RuntimeError(
+                f"Cannot search for {query=} in {collection=}: Collection is empty"
+            )
+
+        results = []
+        for document in documents:
+            results.append(
+                {
+                    "content": document.content,
+                    "score": self.search_engine.get_score(query, document.content),
+                }
+            )
+
+        results.sort(key=lambda x: x["score"], reverse=True)
+        for index, result in enumerate(results):
+            if limit and index >= limit:
+                break
+
+            if result["score"] <= 0:
+                break
+
+            if debug_score:
+                print(f"{index+1}) {result['content']} (score={result['score']})")
+            else:
+                print(f"{index+1}) {result['content']}")
+
+
+def main():
+    fire.Fire(Cli)
+
+
+if __name__ == "__main__":
+    main()
